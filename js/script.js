917401079a841ffbfdf6d93951a23bcfdd7abb4c
Dipesh Yadav <dipeshyadav571999@gmail.com>
2025-03-16 22:19:16 +0530

    next commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/*===============typing effect================*/
+var typed = new Typed('.typing', {  
+    strings: ["","Web Developer", "Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
---
+++ js/style-switcher.js
@@ -0,0 +1,33 @@
+// Toggle style switcher
+const styleSwitcherToggler = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggler.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// // Hide style switcher on scroll
+window.addEventListener("scroll", () => {
+    if (document.querySelector(".style-switcher").classList.contains("open")) {
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
+});
+
+// Theme colors
+const alternateStyles = document.querySelectorAll(".alternate-style");
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
+
+// Day/Night toggle
+const dayNight = document.querySelector(".day-night");
+dayNight.addEventListener("click", () => {
+    dayNight.querySelector("i").classList.toggle("fa-sun");
+    dayNight.querySelector ("i").classList.toggle("fa-moon");
+    document.body.classList.toggle("dark"); // toggle body class
+}
+);
